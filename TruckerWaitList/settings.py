56c717df7f010812475b905a617c19a5b8379dac
aaric <aaricatays@gmail.com>
2025-02-09 05:35:45 -0600

    Fixed DEBUG env variable

--- TruckerWaitList/settings.py
+++ TruckerWaitList/settings.py
@@ -21,7 +21,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "default-secret-key")
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = os.getenv("DEBUG", "False").lower() in ["true", "1"]
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "level1-production.up.railway.app,.railway.app,localhost,127.0.0.1").split(",")
 CSRF_TRUSTED_ORIGINS = [
     "https://level1-production.up.railway.app",  # Be sure to replace with actual domain
